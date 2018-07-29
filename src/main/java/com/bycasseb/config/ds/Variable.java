ca58c68609ffa43493607be403e66d6b7485d4db
casseb <felipe.casseb@gmail.com>
2018-07-29 12:19:45 -0300

    Refatorado código baseado na análise do IntelliJ

--- src/main/java/com/bycasseb/config/ds/Variable.java
+++ src/main/java/com/bycasseb/config/ds/Variable.java
@@ -2,7 +2,6 @@ package com.bycasseb.config.ds;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
